90020018a7aa5ed348dc7f3bc565e8ddea5a33a5
SubhadeepAqb <subhadeep.s@aqbsolutions.com>
2022-03-14 22:28:36 +0530

    Instance of DbModel Created in Controller

--- core/Controller.php
+++ core/Controller.php
@@ -2,8 +2,25 @@
 
 namespace app\core;
 
+use app\models\DbModel;
+
 class Controller
 {
+
+    ////    Object variable of DB_model Class ///
+    protected $dbmodel;
+
+    /////    Creating an instance of DBModel Class  //////
+
+    public function __construct(){
+        // echo phpinfo();
+        // die();
+
+        $this->dbmodel = new DbModel();
+
+
+
+    }
     public string $layout = 'main';
 
     public function setLayout($layout)
