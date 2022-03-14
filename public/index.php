d43677282eb0ab69c7a252ab96e4214aef09d0fe
SubhadeepAqb <subhadeep.s@aqbsolutions.com>
2022-03-14 22:38:16 +0530

    Modified

--- public/index.php
+++ public/index.php
@@ -1,11 +1,12 @@
-<?php  
-
+<?php
+require_once __DIR__ . "/../core/config.php";
 require_once __DIR__ . "/../vendor/autoload.php";
 
 use app\controllers\SiteController;
 use app\core\Application;
 use app\controllers\AuthController;
-
+//print_r($_SERVER['REQUEST_URI']);
+//exit;
 $app = new Application(dirname(__DIR__));
 
 $app->router->get('/', [SiteController::class , 'home']);
@@ -24,7 +25,8 @@ $app->router->get('/register', [AuthController::class, 'register']);
 
 $app->router->post('/register', [AuthController::class, 'register']);
 
-$app->router->get('/a', 'home');
+
+$app->router->get('/app', 'home');
 
 
 
