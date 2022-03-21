74bdfe474c612459b5338e8296472018dcacadec
SubhadeepAqb <subhadeep.s@aqbsolutions.com>
2022-03-21 16:48:43 +0530

    something changed

--- core/config.php
+++ core/config.php
@@ -1,11 +1,10 @@
 <?php
-
 date_default_timezone_set('Asia/Kolkata');
 
 define("DB_HOST", "localhost");
 define("DB_USER", "postgres");
 define("DB_PASS", "admin");
-define("DB_NAME", "subhadeep");
+define("DB_NAME", "tt0");
 
 
 ?>
