ad2d86ae72920f6462c74cfb21795f03bbb3d373
ankurAqb <ankur.g@aqbsolutions.com>
2022-03-23 19:25:05 +0530

    file2

--- core/Controller.php
+++ core/Controller.php
@@ -8,22 +8,7 @@ use app\core\business_object\Location\location_bo;
 class Controller
 {
 
-    ////    Object variable of DB_model Class ///
-    //protected $dbmodel;
-    //public $bo;
-
-    /////    Creating an instance of DBModel Class  //////
-
-    public function __construct(){
-        // echo phpinfo();
-        // die();
-
-        //$this->dbmodel = new DbModel();
-        //
-
-
-
-    }
+    
     public string $layout = 'main';
 
     public function setLayout($layout)
