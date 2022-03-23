81a4a7b6011cd2af80ef5f73d707c5102b0983eb
ankurAqb <ankur.g@aqbsolutions.com>
2022-03-23 19:26:13 +0530

    file3

--- core/business_object/Location/location_bo.php
+++ core/business_object/Location/location_bo.php
@@ -16,7 +16,7 @@ class location_bo extends bo
     
     
     
-    //public $is_delete = false;
+    public $is_delete = '';
 
     
     
@@ -38,7 +38,7 @@ class location_bo extends bo
 
     public function get()
     {
-
+        
     }
 
 
@@ -50,9 +50,25 @@ class location_bo extends bo
 
     public function save()
     {
-        $sql = "INSERT INTO locations (location_name, is_delete) VALUES ('$this->name', false)";
-        $this->db->queryExecute($sql);
-        print "Inserted Successfully";
+        $this->is_delete = 'false';
+        $sql = "INSERT INTO locations (location_name, is_delete) VALUES ('$this->name', '$this->is_delete')";
+        if($last_id = $this->db->queryExecute($sql))
+        {
+        //print "Inserted Successfully";
+            $response = [
+                "success" => true,
+                "msg" => "Inserted Successfully =>".$last_id
+            ];
+            return $response;
+        }
+        else
+        {
+            $response = [
+                "success" => false,
+                "msg" => "Cannot be inserted"
+            ];
+            return $response;
+        }
     }
 
 
@@ -67,6 +83,16 @@ class location_bo extends bo
     {
         
     }
+
+
+
+
+
+
+
+
+
+
 
     public function delete()
     {
