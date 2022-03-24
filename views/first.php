ce8a0cb615cce22c98e7504f0598fb511521abf0
apurbaAqb <101314895+apurbaAqb@users.noreply.github.com>
2022-03-24 10:22:45 +0530

    Login First message Page

---
+++ views/first.php
@@ -0,0 +1,8 @@
+<h1>Authentication Page</h1>
+
+<h3>Welcome <?php print $name ;?></h3>
+
+<br>
+<h4> Please Login First </h4>
+
+<h5> If You are Visiting First Time Then Register Yourself First</h5>
