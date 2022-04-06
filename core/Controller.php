4cc837be1d8cbe3a90a25ff6b037246b0493ba48
apurbaAqb <apurba.d@aqbsolutions.com>
2022-04-06 13:49:40 +0530

    Main Controller Updated

--- core/Controller.php
+++ core/Controller.php
@@ -3,19 +3,34 @@
 namespace app\core;
 
 use app\models\DbModel;
+use app\core\InboundController;
 use app\core\business_object\Location\location_bo;
 
 class Controller
 {
 
     
-    public string $layout = 'main';
+    public string $layout;
+
+    public function __construct()
+    {
+        if(isset($_COOKIE['loginTrue']) && $_COOKIE['loginTrue']!='undefined' && $_COOKIE['loginTrue']!=null 
+             && $_COOKIE['loginTrue']>=1647450336 ){
+
+            $this->layout='main';
+        }
+
+        else{
+            $this->layout='auth';
+        }
+    }
+
+
 
     public function setLayout($layout)
     {
         $this->layout = $layout;
     }
-
     public function render($view, $params = [])
     {
         return Application::$app->router->renderView($view, $params);
